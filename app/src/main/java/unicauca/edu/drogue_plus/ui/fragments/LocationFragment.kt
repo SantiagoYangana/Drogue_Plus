bbe5f7fa5a8839565a3e18ffbd684bef87d17bf8
pintocristian <pintocristian@unicauca.edu.co>
2022-12-05 20:18:14 -0500

    Fix Mapa
    Ya se despliega el mapa

--- app/src/main/java/unicauca/edu/drogue_plus/ui/fragments/LocationFragment.kt
+++ app/src/main/java/unicauca/edu/drogue_plus/ui/fragments/LocationFragment.kt
@@ -5,13 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import unicauca.edu.drogue_plus.R
 import unicauca.edu.drogue_plus.databinding.FragmentHomeBinding
 import unicauca.edu.drogue_plus.databinding.FragmentLocationBinding
 
 
-class LocationFragment : Fragment() {
+class LocationFragment : Fragment(), OnMapReadyCallback {
 
+    private lateinit var nMap: GoogleMap
     private var _binding: FragmentLocationBinding? = null
     private val binding : FragmentLocationBinding get() = _binding!!
 
@@ -23,4 +30,25 @@ class LocationFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStart() {
+        super.onStart()
+        val mapFragment: SupportMapFragment = childFragmentManager.findFragmentById(R.id.fragment_location_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+    override fun onMapReady(map: GoogleMap) {
+        nMap=map
+        nMap.uiSettings .isScrollGesturesEnabled =true
+        nMap.uiSettings .isZoomControlsEnabled=true
+        nMap.uiSettings .isZoomGesturesEnabled=true
+        nMap.uiSettings .isCompassEnabled =true
+        nMap.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        nMap.isTrafficEnabled=true
+
+        val lating: LatLng = LatLng(2.4454181795496903, -76.61976473210028)
+
+        nMap.addMarker(
+            MarkerOptions().position(lating)
+            .title("DroguePlus"))
+        nMap.moveCamera(CameraUpdateFactory.newLatLngZoom(lating,12.0f))
+    }
 }
