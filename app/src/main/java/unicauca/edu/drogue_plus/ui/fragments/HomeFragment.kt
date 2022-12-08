1475fc11632c2ddca7f85efc66031384a7479925
Laura Daza <laurasofiadaza12@gmail.com>
2022-12-08 11:08:43 -0500

    Cambios con lista de medicamentos actualizada

--- app/src/main/java/unicauca/edu/drogue_plus/ui/fragments/HomeFragment.kt
+++ app/src/main/java/unicauca/edu/drogue_plus/ui/fragments/HomeFragment.kt
@@ -5,22 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import unicauca.edu.drogue_plus.R
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import unicauca.edu.drogue_plus.databinding.FragmentHomeBinding
+import unicauca.edu.drogue_plus.databinding.FragmentMedicationBinding
+
 
 
 class HomeFragment : Fragment() {
+
     private var _binding: FragmentHomeBinding? = null
-    private val binding : FragmentHomeBinding get() = _binding!!
-    private lateinit var MedicineAdapter: MedicineAdapter
+    private val binding: FragmentHomeBinding get() = _binding!!
+    private lateinit var serviceAdapter: MedicineAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onStart() {
+        super.onStart()
+        serviceAdapter = MedicineAdapter(
+            mutableListOf()
+        )
+
+        serviceAdapter.listener = object : OnMedicineClickListener {
+            override fun onClick(item: MedicineModel) {
+                val action = HomeFragmentDirections.actionHomeFragmentToMedicineFragment22()
+                action.search = false
+                action.name = item.name
+                findNavController().navigate(action)
+            }
+
+        }
+
+    }
+
 
 }
